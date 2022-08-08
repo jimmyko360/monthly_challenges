9ec1a12667d3d444166bfdffb85c21d7ce95893d
Jimmy Ko <jko@thelabnyc.com>
2022-08-08 18:36:49 -0400

    give name to str:month path

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("<int:month_number>", views.challenge_by_month_by_number),
-    path("<str:month>", views.challenge_by_month)
+    path("<str:month>", views.challenge_by_month, name="challenge_of_the_month")
 ]
