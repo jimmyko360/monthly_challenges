f87de77de5aa83d59c4d3804830ec42cdaedb64a
Jimmy Ko <jko@thelabnyc.com>
2023-03-07 17:35:58 -0500

    Section 4 Templates
    create endpoint for an "All Challenges" homepage

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("<int:month_number>", views.challenge_by_month_by_number),
     path("<str:month>", views.challenge_by_month, name="challenge_of_the_month"),
-    path("", views.get_all_months)
+    path("", views.index, name="index")
 ]
