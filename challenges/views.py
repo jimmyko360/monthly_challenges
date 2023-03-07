4ef5e85bc38c35dfa4cfa0c6aa2a8c1889847eb0
Jimmy Ko <jko@thelabnyc.com>
2023-03-07 17:36:47 -0500

    Section 4 Templates
    refactor all HTML into template files

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -16,7 +17,7 @@ monthly_challenges = {
     "september": "laundry",
     "october": "gym",
     "november": "tan",
-    "december": "laundry"
+    "december": None
 }
 
 
@@ -32,13 +33,24 @@ def challenge_by_month_by_number(request, month_number):
 def challenge_by_month(request, month):
     try:
         challenge = monthly_challenges[month]
-        challenge_text = f"<h1>{month}</h1><br><h3>{challenge}</h3>"
-        return HttpResponse(challenge_text)
+
+        # returning this inline HTML is not the best pattern
+        # challenge_text = f"<h1>{month}</h1><br><h3>{challenge}</h3>"
+        # return HttpResponse(challenge_text)
+
+        # a good alternative but still not the most popular pattern
+        # response_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response_data)
+
+        # the Django best practice
+        return render(request, "challenges/challenge.html", {"month_name": month, "challenge_name": challenge})
     except:
-        return HttpResponseNotFound("<h1>This page is located in a different castle.</h1>")
+        # response = render_to_string("404.html")
+        # return HttpResponseNotFound(response)
+        raise Http404()
 
 
-def get_all_months(request):
+def index(request):
     all_months = ""
 
     # for each month from 0 to 12
@@ -47,11 +59,21 @@ def get_all_months(request):
 
     months = monthly_challenges.keys()
 
-    for x in months:
-        month_link = reverse("challenge_of_the_month", args=[x])
-        month_li = f"<li><a href={month_link}>{x}</a><br></li>"
-        all_months = all_months + month_li
+    # we move this for loop into the template and delegate the iterating work to it using a tag
+    # for x in months:
+    #     month_link = reverse("challenge_of_the_month", args=[x])
+    #     month_li = f"<li><a href={month_link}>{x}</a><br></li>"
 
-    month_list = f"<h1>All Challenges</h1><br><ul>{all_months}</ul>"
+    #     # Max defines this href with escaped quotations for some reason
+    #     # e.g. f"href=\'{month_link}\'"
+    #     # it works without them though
+    #     # shouldn't it be fine since month_link is a string anyways?
 
-    return HttpResponse(month_list)
+    #     all_months = all_months + month_li
+
+    # month_list = f"<h1>All Challenges</h1><br><ul>{all_months}</ul>"
+
+    # return HttpResponse(month_list)
+
+    # month_link = reverse("challenge_of_the_month", args=[x])
+    return render(request, "challenges/index.html", {"months": months})
